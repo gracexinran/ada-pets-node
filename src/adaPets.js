aa1b07fe88006a50e44f9d3c6d5d003873d5a306
gracexinran <gracexinran@gmail.com>
2019-11-18 23:08:50 -0800

    Completed

--- src/adaPets.js
+++ src/adaPets.js
@@ -10,6 +10,13 @@ const BASE_URL = "https://petdibs.herokuapp.com/pets/";
 // Option functions.
 const listPets = () => {
   // Fill out as part of Wave 1.
+  axios.get(BASE_URL)
+  .then((response) => {
+    console.log(setResult(response.data))
+  })
+  .catch((error) => {
+    console.log(setError(error.message));
+  });
 }
 
 const showDetails = (selectedPet) => {
@@ -17,8 +24,15 @@ const showDetails = (selectedPet) => {
     setError("You tried to show details for a pet without selecting it!");
     return;
   }
-
+  
   // Fill out as part of Wave 2.
+  axios.get(BASE_URL + `${selectedPet}`)
+  .then((response) => {
+    console.log(setResult(response.data))
+  })
+  .catch((error) => {
+    setError('Failed, and details are:', error.message);
+  });
 }
 
 const removePet = (selectedPet) => {
@@ -26,12 +40,26 @@ const removePet = (selectedPet) => {
     setError("You tried to remove a pet without selecting it!");
     return;
   }
-
+  
   // Fill out as part of Wave 3.
+  axios.delete(BASE_URL + `${selectedPet}`)
+  .then((response) => {
+    console.log(setResult(response.data))
+  })
+  .catch((error) => {
+    console.log(setError('Failed, cannot remove non-existed pet', error.message));
+  });
 }
 
 const addPet = (petInfo) => {
   // Fill out as part of Wave 4.
+  axios.post(BASE_URL, petInfo)
+  .then((response) => {
+    console.log(setResult(response.data))
+  })
+  .catch((error) => {
+    console.log(setError('Failed, cannot add pet', error));
+  });
 }
 
 // Use Node-style exports to export functions for tests and main.
